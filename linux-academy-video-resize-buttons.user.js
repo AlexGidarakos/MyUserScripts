7865ac54ef37af33b94529d2bf1b2cc999c2b15a
Alexandros Gidarakos <algida79@gmail.com>
2016-04-26 01:37:40 +0300

    Disable buttons when size limits have been reached

--- linux-academy-video-resize-buttons.user.js
+++ linux-academy-video-resize-buttons.user.js
@@ -11,7 +11,7 @@
 // @author      Alexandros Gidarakos <algida79@gmail.com>
 // @name        Linux Academy - Video resize buttons
 // @namespace   linuxacademy.com
-// @version     0.1
+// @version     0.2
 // @description Adds video resize buttons on Linux Academy course pages
 // @homepage    https://github.com/AlexGidarakos/MyUserScripts
 // @include     https://linuxacademy.com/cp/courses/lesson/course/*
@@ -23,12 +23,16 @@ var target = $(".la-video-player").parent();
 function smallerVid() {
   var size = target.attr("class").split('-')[2];
   if(size > 4) size--;
+  if(size == 4) $("#smallerBtn").toggleClass("disabled");
+  if(size == 11) $("#largerBtn").toggleClass("disabled");
   target.attr("class", "col-md-" + size);
 }
 
 function largerVid() {
   var size = target.attr("class").split('-')[2];
   if(size < 12) size++;
+  if(size == 5) $("#smallerBtn").toggleClass("disabled");
+  if(size == 12) $("#largerBtn").toggleClass("disabled");
   target.attr("class", "col-md-" + size);
 }
 
